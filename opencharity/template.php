d169556c7f685cc54ff8190ea7fd33068895b11e
ehsanham <ehsanhamidi@gmail.com>
2017-01-18 00:02:40 +0330

    Add files via upload

--- opencharity/template.php
+++ opencharity/template.php
@@ -12,3 +12,14 @@ function opencharity_menu_tree($variables) {
     return '<ul class="nav navbar-nav">' . $variables['tree'] . '</ul>';
   
 }
+
+function opencharity_preprocess_html($variables) {
+$viewport = array(
+'#tag' => 'meta',
+'#attributes' => array(
+'name' => 'viewport',
+'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+),
+);
+drupal_add_html_head($viewport, 'viewport');
+}
